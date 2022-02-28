87102810757ba767ec37b8bda398f003211c7c4e
Camilo García <72902253+camilogs1@users.noreply.github.com>
2022-02-27 23:00:53 -0500

    Upload points

---
+++ Seguimiento2/Punto17/Punto17/Punto17.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Punto17
+{
+    class Punto17
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            int suma = 0;
+            while(num < 5)
+            {
+                num = num + 1;
+                suma = suma + num;
+            }
+            Console.WriteLine("La suma es: " + suma);
+        }
+    }
+}
