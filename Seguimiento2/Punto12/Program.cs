bbc41af5136055eea9a6f3c912ce04ff2b6bf6f8
Sneyder <sneider.caicedo@gmail.com>
2022-02-28 19:02:49 -0500

    Create point 12

---
+++ Seguimiento2/Punto12/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Punto12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String NOM;
+            int NHT, HET, HEE8;
+            double VHN, SALARIO;
+
+            NOM = "elias jose";
+            NHT = 53;
+            VHN = 4000;
+
+            if (NHT > 40)
+            {
+                HET = NHT - 40;
+                if (HET > 8)
+                {
+                    HEE8 = HET - 8;
+                    SALARIO = (40 * VHN) + (16 * VHN) + (HEE8 * 3 * VHN);
+                }
+                else
+                    SALARIO = (40 * VHN) + (HET * 2 * VHN);
+            }
+            else
+                SALARIO = NHT * VHN;
+
+            Console.WriteLine("El trabajador, "+NOM+" Devengp: $"+SALARIO);
+        }
+    }
+}
