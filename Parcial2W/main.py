d87c4d9370562a5816078c5523e50c6e7f9c86ec
Sneyder <sneider.caicedo@gmail.com>
2022-05-09 19:24:01 -0500

    Parcial 2 parte 2

--- Parcial2W/main.py
+++ Parcial2W/main.py
@@ -1,5 +1,14 @@
 from os import system
 from selenium import webdriver
 
-driver = webdriver.Firefox(executable_path=r"D:\Universidad Catolica Luis Amigo\Semestre 7\Verificacion & Validacion de Software\QA_Testing\Parcial2\geckodriver.exe")
-driver.get("https://dev.to")
+driver = webdriver.Firefox(executable_path=r"D:\Universidad Catolica Luis Amigo\Semestre 7\Verificacion & Validacion de Software\QA_Testing\Parcial2W\geckodriver.exe")
+driver.get("https://www.facebook.com/")
+driver.maximize_window()
+
+email = driver.find_element_by_name("email")
+password = driver.find_element_by_name("pass")
+
+email.send_keys('abc@gmail.com')
+password.send_keys('12345678')
+
+driver.find_element_by_name('login').click()
